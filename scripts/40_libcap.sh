ab72c008f6a55d7dd3ed45ce444d53d0e4cfa741
Den <98147657+Arch91@users.noreply.github.com>
2022-01-26 17:00:26 +0300

    Linux from scratch script
    https://github.com/frno7/linux/wiki/building-programs-for-PlayStation-2-Linux

---
+++ scripts/40_libcap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Simple brutal sh-scripts chain.
+if [ "$(echo $PATH | grep -o /usr/local/ps2/bin)" != "/usr/local/ps2/bin" ]
+then
+ echo -en "\033[36;1m There is no "/usr/local/ps2/bin" was added to \$PATH . Adding... \033[0m\n"
+ export PATH=/usr/local/ps2/bin:$PATH
+fi
+
+cd ../sources
+if [ ! -e libcap-2.24.tar.xz ]
+then
+wget https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.24.tar.xz || exit -1
+fi
+cd ../building
+tar -Jxvf ../sources/libcap-2.24.tar.xz
+cd libcap-2.24
+
+patch -p1 < ../../patches/40_libcap-2.24_compat.patch || exit -1
+
+make -j 4 || exit -1
+make install
+# Ignore those afterinstallation launches. Libs and the appropriate are installed.
+
+sleep 2
+
+cd ../../scripts
+sh 41_zlib.sh
