8b68ba87c2d4aea4d8b2fd3d8636de806324ffad
Den <98147657+Arch91@users.noreply.github.com>
2022-01-26 16:44:31 +0300

    Linux from scratch script
    https://github.com/frno7/linux/wiki/building-programs-for-PlayStation-2-Linux

---
+++ scripts/16_findutils.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Simple brutal sh-scripts chain.
+if [ "$(echo $PATH | grep -o /usr/local/ps2/bin)" != "/usr/local/ps2/bin" ]
+then
+ echo -en "\033[36;1m There is no "/usr/local/ps2/bin" was added to \$PATH . Adding... \033[0m\n"
+ export PATH=/usr/local/ps2/bin:$PATH
+fi
+
+cd ../sources
+if [ ! -e findutils-4.7.0.tar.xz ]
+then
+wget https://ftp.gnu.org/gnu/findutils/findutils-4.7.0.tar.xz || exit -1
+fi
+cd ../building
+tar -Jxvf ../sources/findutils-4.7.0.tar.xz
+cd findutils-4.7.0
+
+./configure --prefix=/usr/local/ps2/mipsr5900el-unknown-linux-gnu --host=mipsr5900el-unknown-linux-gnu || exit -1
+
+make -j 4 || exit -1
+make install || exit -1
+
+sleep 2
+
+cd ../../scripts
+sh 17_gawk.sh
