3dc75a741f7d6627bb5ae86a375984e7110b2755
Den <98147657+Arch91@users.noreply.github.com>
2022-01-26 16:49:05 +0300

    Linux from scratch script
    https://github.com/frno7/linux/wiki/building-programs-for-PlayStation-2-Linux

---
+++ scripts/24_patch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Simple brutal sh-scripts chain.
+if [ "$(echo $PATH | grep -o /usr/local/ps2/bin)" != "/usr/local/ps2/bin" ]
+then
+ echo -en "\033[36;1m There is no "/usr/local/ps2/bin" was added to \$PATH . Adding... \033[0m\n"
+ export PATH=/usr/local/ps2/bin:$PATH
+fi
+
+cd ../sources
+if [ ! -e patch-2.7.tar.xz ]
+then
+wget https://ftp.gnu.org/gnu/patch/patch-2.7.tar.xz || exit -1
+fi
+cd ../building
+tar -Jxvf ../sources/patch-2.7.tar.xz
+cd patch-2.7
+
+patch -p1 < ../../patches/24_patch-2.7-incfix.patch || exit -1
+
+./configure --prefix=/usr/local/ps2/mipsr5900el-unknown-linux-gnu --host=mipsr5900el-unknown-linux-gnu || exit -1
+
+make -j 4 || exit -1
+make install || exit -1
+
+sleep 2
+
+cd ../../scripts
+sh 25_sed.sh
